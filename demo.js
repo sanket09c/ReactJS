bfbe0ac43e73f279ab6ad144f3f7e3c7d41a35a0
sanket09c <sanket09c@gmail.com>
2020-03-01 03:55:46 -0500

    added by suhas

--- demo.js
+++ demo.js
@@ -6,4 +6,4 @@ Checkout : -
         -- git checkout filename = remove all file changes which are not committed
     2) branch checkout : 
         -- git checkout branchname = used to switch between branches
-    
+    3) Added by suhas
